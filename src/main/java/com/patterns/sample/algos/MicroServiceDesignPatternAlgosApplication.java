1ddab5a2833e5d3103db437fe536ff30fe0e3fd0
Jitender.Yadav <jy2054050@gmail.com>
2025-02-09 08:27:15 +0530

    initial setup

---
+++ src/main/java/com/patterns/sample/algos/MicroServiceDesignPatternAlgosApplication.java
@@ -0,0 +1,13 @@
+package com.patterns.sample.algos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceDesignPatternAlgosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceDesignPatternAlgosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patterns/sample/algos/MicroServiceDesignPatternAlgosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patterns.sample.algos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceDesignPatternAlgosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
